21788c42aa10063165bd4260f098540238d6dad2
Frank Poon <fpooooon@gmail.com>
2017-12-22 17:14:57 -0800

    convert to https + check mysql values

--- functions/server.js
+++ functions/server.js
@@ -8,6 +8,8 @@ import http from 'http';
 import https from 'https';
 import mysql from 'mysql';
 import bcrypt from 'bcrypt';
+import passport from 'passport';
+import LocalStrategy from 'passport-local.Strategy'
 
 // Constants set for running the server
 const app = express();
@@ -41,6 +43,11 @@ app.use(require('webpack-dev-middleware')(compiler, {
     publicPath: config.output.publicPath
 }));
 
+passport.user(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}));
+
 // Handles loading GET
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
@@ -52,8 +59,18 @@ app.get('/account', (req, res) => {
 
 // Handles loading POST
 app.post('/login', (req, res) => {
-    res.send('/login POST request');
-    console.log(req.body);
+    connection.connect();
+    connection.query(
+        'SELECT * FROM Users WHERE Email = ?',
+        [req.body.email],
+        (error, results, fields) => {
+            if (error) console.log(error);
+            console.log(results);
+            console.log('field below');
+            console.log(fields);
+        }
+    )
+    connection.end();
 });
 
 app.post('/register', (req, res) => {
@@ -61,7 +78,8 @@ app.post('/register', (req, res) => {
 
     bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
         connection.connect();
-        connection.query('INSERT INTO Users (Name, Email, PasswordHash) VALUES (?, ?, ?)',
+        connection.query(
+            'INSERT INTO Users (Name, Email, PasswordHash) VALUES (?, ?, ?)',
             [req.body.name, req.body.email, hash],
             (error, results, fields) => {
                 if (error) console.log(error);
@@ -72,12 +90,15 @@ app.post('/register', (req, res) => {
 });
 
 // starts server in both HTTP and HTTPS (may remove HTTP later)
-var httpServer = http.createServer(app);
+// var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials , app);
 
+/*
 httpServer.listen(HTTPPORT, () => {
     console.log('HTTP server started at ' + HTTPPORT);
 });
+*/
+
 httpsServer.listen(HTTPSPORT, () => {
     console.log('HTTPS server started at ' + HTTPSPORT);
 });
